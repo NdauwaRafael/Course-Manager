2d9cf107042ea4fc84ef5d8980d073781d7cd251
Raphael Karanja <rkaranja@cytonn.com>
2019-01-19 15:18:33 +0300

    UPDATE: Add basic router links in already created header component

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,3 +1,10 @@
 /**
  * Created by Raphael Karanja on 2019-01-19.
  */
+import React from 'react';
+import {Link, IndexLink} from 'react-router'
+export default (props)=>
+    <nav>
+        <IndexLink to="/" >Home</IndexLink>
+        <Link to="/about">About</Link>
+    </nav>
