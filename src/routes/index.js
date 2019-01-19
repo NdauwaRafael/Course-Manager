c8a931488692b616d12b6122a7ffebe49a28e811
Raphael Karanja <rkaranja@cytonn.com>
2019-01-19 11:52:25 +0300

    UPDATE: Add basic Route configuration logic in place

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,15 @@
 /**
  * Created by Raphael Karanja on 2019-01-19.
  */
+import React from 'react';
+import { Route, IndexRoute} from  'react-router';
+import App from '../components/App';
+import HomePage from '../components/Home';
+import AboutPage from '../components/About';
+
+export default {
+<Route path='/' component={App}>
+    <IndexRoute  component={HomePage} />
+    <Route path="about" component={AboutPage} />
+</Route>
+}
