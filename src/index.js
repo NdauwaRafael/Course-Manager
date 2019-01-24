779ef1ff00d27cf2bbddc0aa60db4cc13bfdbafa
Raphael Karanja <rkaranja@cytonn.com>
2019-01-24 07:40:54 +0300

    UPDATE: Add tailwind css to the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'tailwindcss/dist/tailwind.min.css';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
