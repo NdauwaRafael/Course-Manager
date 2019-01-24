4f95f8353c0ed82c12fd8efa0d518193c6a0ccbf
Raphael Karanja <rkaranja@cytonn.com>
2019-01-24 07:26:37 +0300

    UPDATE: Add basic data for courses page

--- src/components/Course/Courses/index.js
+++ src/components/Course/Courses/index.js
@@ -3,5 +3,13 @@
  */
 import React, {Component} from 'react';
 export default class extends Component {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            courses: [],
+            course: {
+                title: null
+            }
+        }
+    }
 }
