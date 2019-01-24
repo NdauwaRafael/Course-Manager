e807249445d3169d9cf6c94071f33d82968b583c
Raphael Karanja <rkaranja@cytonn.com>
2019-01-24 12:26:49 +0300

    UPDATE:  Configure store and instantiate store instance in appliucation entry point

--- src/CourseAppStore/configureSore/index.js
+++ src/CourseAppStore/configureSore/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by Raphael Karanja on 2019-01-24.
  */
+import {createStore, applyMiddleware} from "redux";
+import rootReducer from '../reducers/CourseReducer';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+
+export default (initialState)=>{
+    return createStore(
+        initialState,
+        rootReducer,
+        applyMiddleware(reduxImmutableStateInvariant())
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import  './assets/css/file.scss';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import App from './components/App';
 
-import App from './components/App'
-
+import configureStore from './CourseAppStore/configureSore'
+const store = configureStore();
 render(
     <BrowserRouter>
         <App />
