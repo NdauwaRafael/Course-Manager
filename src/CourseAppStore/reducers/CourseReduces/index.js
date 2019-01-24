f6193ae1ddb551db30661d18b5d2baeaa61c8065
Raphael Karanja <rkaranja@cytonn.com>
2019-01-24 11:39:56 +0300

    UPDATE:  Return a mutated state

--- src/CourseAppStore/reducers/CourseReduces/index.js
+++ src/CourseAppStore/reducers/CourseReduces/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by Raphael Karanja on 2019-01-24.
  */
+export default (state=[], action)=>{
+    switch (action.type) {
+        case 'CREATE_COURSE':
+                return [
+                    ...state,
+                    Object.assign({},action.course )
+                ];
+        default:
+            return state;
+    }
+}
