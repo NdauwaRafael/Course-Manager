dbb5daf668ee0d8c342b0c965fba9ab1ab8a9297
Raphael Karanja <rkaranja@cytonn.com>
2019-01-24 08:24:20 +0300

    UPDATE:  Style add course form

--- src/components/Course/Courses/index.js
+++ src/components/Course/Courses/index.js
@@ -17,44 +17,49 @@ export default class extends Component {
     render() {
         return (
             <div>
+                <div className="text-grey-dark flex items-center mb-5">
+                    Courses
+                </div>
+                <div className="text-black font-bold text-xl mb-5">Add a Course</div>
                 <form className="w-full max-w-md">
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <div className="flex flex-wrap -mx-3 mb-10">
+                        <div className="w-full px-3">
                             <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
                                    htmlFor="grid-first-name">
                                 Title
                             </label>
                             <input
                                 className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                                id="grid-first-name" type="text" placeholder="Jane" />
+                                id="grid-first-name" type="text" placeholder="Introduction to Redux" />
                                 <p className="text-red text-xs italic">Please fill out this field.</p>
                         </div>
-                        <div className="w-full md:w-1/2 px-3">
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-10">
+                        <div className="w-full px-3 ">
                             <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
                                    htmlFor="grid-last-name">
                                 Category
                             </label>
                             <input
                                 className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey"
-                                id="grid-last-name" type="text" placeholder="Doe" />
+                                id="grid-last-name" type="text" placeholder="Programming" />
                         </div>
                     </div>
-                    <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="flex flex-wrap -mx-3 mb-10">
                         <div className="w-full px-3">
                             <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
                                    htmlFor="grid-password">
                                 Description
                             </label>
-                            <input
+                            <textarea
                                 className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-grey"
-                                id="grid-password" type="password" placeholder="******************" />
+                                id="grid-password"  placeholder="This is a demo description of a course." />
                                 <p className="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
                         </div>
                     </div>
 
                     <div className="md:flex md:items-center">
-                        <div className="md:w-1/3"> </div>
-                        <div className="md:w-2/3">
+                        <div>
                             <button
                                 className="shadow bg-purple hover:bg-purple-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                                 type="button">
