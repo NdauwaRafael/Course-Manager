191b7875df8abb48a0e81f9b1af46f4bbaf53357
Raphael Karanja <rkaranja@cytonn.com>
2019-01-25 10:23:53 +0300

    UPDATE:  Add thunk to store

--- src/CourseAppStore/configureSore/index.js
+++ src/CourseAppStore/configureSore/index.js
@@ -3,13 +3,14 @@
  */
 import rootReducer from '../reducers';
 import {createStore, applyMiddleware} from "redux";
-import reduxImmutableStatateInvariant from 'redux-immutable-state-invariant'
+import reduxImmutableStatateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 const configureStore = (initialState)=>{
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(reduxImmutableStatateInvariant)
+        applyMiddleware(thunk, reduxImmutableStatateInvariant)
     );
 };
 export default configureStore;
