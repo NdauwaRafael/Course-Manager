80721452390de8fbb24f7bc8247b5bef023edac1
Raphael Karanja <rkaranja@cytonn.com>
2019-01-25 10:22:20 +0300

    UPDATE:  Add apply middlware configurations to configurestore file

--- src/CourseAppStore/configureSore/index.js
+++ src/CourseAppStore/configureSore/index.js
@@ -2,11 +2,14 @@
  * Created by Raphael Karanja on 2019-01-24.
  */
 import rootReducer from '../reducers';
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
+import reduxImmutableStatateInvariant from 'redux-immutable-state-invariant'
 
 const configureStore = (initialState)=>{
     return createStore(
-        rootReducer
+        rootReducer,
+        initialState,
+        applyMiddleware(reduxImmutableStatateInvariant)
     );
 };
 export default configureStore;
