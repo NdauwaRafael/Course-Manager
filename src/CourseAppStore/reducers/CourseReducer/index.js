1364123106ca7588e3fe425605dd9aff7f2bfa76
Raphael Karanja <rkaranja@cytonn.com>
2019-01-25 11:13:54 +0300

    UPDATE:  Load action on reducer

--- src/CourseAppStore/reducers/CourseReducer/index.js
+++ src/CourseAppStore/reducers/CourseReducer/index.js
@@ -2,13 +2,16 @@
  * Created by Raphael Karanja on 2019-01-24.
  */
 import * as types from '../../actions/ActionTypes'
-export default (state=[], action)=>{
+
+export default (state = [], action) => {
     switch (action.type) {
         case types.CREATE_COURSE:
-                return [
-                    ...state,
-                    Object.assign({}, action.course )
-                ];
+            return [
+                ...state,
+                Object.assign({}, action.course)
+            ];
+        case types.LOAD_COURSES_SUCCESS:
+            return action.courses;
         default:
             return state;
     }
