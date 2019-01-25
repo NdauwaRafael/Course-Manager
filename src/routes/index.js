727ce9b6666d961e6efd9df27a62833c6e5dc78a
Raphael Karanja <rkaranja@cytonn.com>
2019-01-25 15:58:00 +0300

    UPDATE:  Add a manage course route configurations

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,14 @@ import {Route, Switch} from 'react-router-dom';
 import HomePage from '../components/Home';
 import AboutPage from '../components/About';
 import CoursesPage from '../components/Course/Courses';
+import ManageCourse from '../components/Course/Courses/ManageCourse'
 
 export default (
     <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/course" component={ManageCourse} />
+        <Route path="/course/:id" component={ManageCourse} />
         <Route path="/courses" component={CoursesPage} />
     </Switch>
 )
