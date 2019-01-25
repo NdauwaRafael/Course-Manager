96d04008caf3f89337b43c30981bd1f6a67ffde7
Raphael Karanja <rkaranja@cytonn.com>
2019-01-25 15:10:56 +0300

    UPDATE:  Create a manage courses page

---
+++ src/components/Course/Courses/ManageCourses/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Raphael Karanja on 2019-01-25.
+ */
+import React, {Component} from 'react';
+
+export default class extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
