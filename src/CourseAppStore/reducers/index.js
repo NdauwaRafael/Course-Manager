59d1b57ad4ff4343ec9043c0d849e53f718bbd8f
Raphael Karanja <rkaranja@cytonn.com>
2019-01-28 10:22:11 +0300

    UPDATE:  Add authorsReducer to root reducer

--- src/CourseAppStore/reducers/index.js
+++ src/CourseAppStore/reducers/index.js
@@ -2,10 +2,12 @@
  * Created by Raphael Karanja on 2019-01-24.
  */
 import {combineReducers} from 'redux';
-import courses from './CourseReducer'
+import courses from './CourseReducer';
+import authors from './AuthorReducer'
 
 const rootReducer = combineReducers({
-    courses
+    courses,
+    authors
 });
 
 export default rootReducer;
