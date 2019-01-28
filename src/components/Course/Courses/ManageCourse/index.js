cb3d15a1972df97305799adedc1327b5aa17518b
Raphael Karanja <rkaranja@cytonn.com>
2019-01-28 08:57:04 +0300

    UPDATE:  Load Addcourse component to manage course component

--- src/components/Course/Courses/ManageCourse/index.js
+++ src/components/Course/Courses/ManageCourse/index.js
@@ -6,22 +6,60 @@ import * as courseActions from '../../../../CourseAppStore/actions/CourseActions
 import {bindActionCreators} from "redux";
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux'
+import {alert, confirm} from "notie";
+import AddCourse from './CourseForm'
 
 class ManageCourses extends Component {
     constructor(props, context){
         super(props, context);
 
         this.state = {
+            course: {
+                title: '',
+                category: '',
+                author: '',
+                description: ''
+            }
+        };
 
-        }
+        this.handleChange = this.handleChange.bind(this);
+        this.onSave = this.onSave.bind(this);
+    };
+
+    handleChange(event) {
+        let field = event.target.name;
+        let value = event.target.value;
+        let course = this.state.course;
+        course[field] = value;
+        this.setState({course});
+    };
+
+    onSave() {
+        const {course} = this.state;
+        confirm({
+            text: 'Are you sure you want to do that?<br><b>That\'s a bold move...</b>',
+            cancelCallback: () => {
+                alert({type: 3, text: 'Aw, why not? :(', time: 2})
+            },
+            submitCallback: () => {
+                this.props.actions.createCourse(course);
+            }
+        })
     };
 
     render() {
+        const {allAuthors, errors} = this.props;
+        const {course} = this.state;
         return (
             <div>
                 <h1>
                     Manage Page
                 </h1>
+                <AddCourse onChange={this.handleChange}
+                           course={course}
+                           allAuthors={allAuthors}
+                           onSave={this.onSave}
+                           errors={errors}/>
             </div>
         );
     }
