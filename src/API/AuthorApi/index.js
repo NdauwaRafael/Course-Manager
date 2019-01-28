e27791efdfa98366e5dc1ec1b18b3d04be6dded5
Raphael Karanja <rkaranja@cytonn.com>
2019-01-28 10:08:53 +0300

    UPDATE:  Create a thunk for loading all authors and returning an action with the authors data

--- src/API/AuthorApi/index.js
+++ src/API/AuthorApi/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by Raphael Karanja on 2019-01-28.
  */
+
+let getAllAuthors =  async function () {
+    return await (await (await (fetch('http://localhost:3004/authors')))).json();
+    // return fetch('http://localhost:3004/courses')
+};
+
+export default {
+    getAllAuthors
+}
--- src/CourseAppStore/actions/AuthorActions/index.js
+++ src/CourseAppStore/actions/AuthorActions/index.js
@@ -2,4 +2,22 @@
  * Created by Raphael Karanja on 2019-01-28.
  */
 import * as types from '../ActionTypes'
-import courseAPI from  '../../../API/AuthorApi';
+import authorAPI from  '../../../API/AuthorApi';
+
+export function loadAuthorsSuccess(authors) {
+    return {
+        type: types.LOAD_AUTHORS_SUCCESS,
+        authors
+    }
+}
+
+export function loadAuthors() {
+    return function (dispatch) {
+        return authorAPI.getAllAuthors()
+            .then(authors=>{
+                dispatch(loadAuthorsSuccess(authors))
+            }, error=>{
+
+            })
+    }
+}
