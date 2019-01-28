5f99a54328b7766e58d3b9f7a39e07399ece38f6
Raphael Karanja <rkaranja@cytonn.com>
2019-01-28 09:52:57 +0300

    UPDATE:  Add card to encase manage courses form

--- src/components/Course/Courses/ManageCourse/index.js
+++ src/components/Course/Courses/ManageCourse/index.js
@@ -47,16 +47,26 @@ class ManageCourses extends Component {
         const {allAuthors} = this.props;
         const {course, errors} = this.state;
         return (
-            <div>
-                <h1>
-                    Manage Page
-                </h1>
-                <AddCourse onChange={this.handleChange}
-                           course={course}
-                           allAuthors={[]}
-                           onSave={this.onSave}
-                           errors={errors}/>
-            </div>
+                <div className="max-w-md w-full">
+                    <div
+                        className="border-r border-b border-l border-grey-light lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-l lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                        <div className="mb-8">
+                            <p className="text-sm text-grey-dark flex items-center">
+                                <svg className="fill-current text-grey w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/>
+                                </svg>
+                                Manage Authors Page
+                            </p>
+                            <AddCourse onChange={this.handleChange}
+                                       course={course}
+                                       allAuthors={[]}
+                                       onSave={this.onSave}
+                                       errors={errors}/>
+                        </div>
+                    </div>
+                </div>
         );
     }
 }
