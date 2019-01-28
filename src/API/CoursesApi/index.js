fea898198d6d5440fa7e803a8e0c5766d1f7e423
Raphael Karanja <rkaranja@cytonn.com>
2019-01-28 15:10:54 +0300

    UPDATE:  Add api link referencing where the course data will be saved

--- src/API/CoursesApi/index.js
+++ src/API/CoursesApi/index.js
@@ -5,7 +5,15 @@ let getAllCourses =  async function () {
     return await (await (await (fetch('http://localhost:3004/courses')))).json();
 };
 let createCourse = function (course) {
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(course)
+    };
 
+    return fetch('http://localhost:3004/courses', options);
 }
 
 export default {
