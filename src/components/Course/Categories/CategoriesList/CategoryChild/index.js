070de8881e145b211cae81e82f750957652b41e6
Raphael Karanja <rkaranja@cytonn.com>
2019-01-30 10:37:16 +0300

    UPDATE:  Add key to category child

--- src/components/Course/Categories/CategoriesList/CategoryChild/index.js
+++ src/components/Course/Categories/CategoriesList/CategoryChild/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 const CategoryChild = ({category}) => {
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg" key={category.id}>
             <img className="w-full" src="https://via.placeholder.com/468x60?text=Course+Categories" alt="Sunset in the mountains" />
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{category.name}</div>
