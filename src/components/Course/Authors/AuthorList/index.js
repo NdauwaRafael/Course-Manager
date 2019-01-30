00cfa19f575eba3b19f3ea33883ce853ef55839a
Raphael Karanja <rkaranja@cytonn.com>
2019-01-30 09:25:49 +0300

    UPDATE:  Add Categorylist anb author list stateless components

---
+++ src/components/Course/Authors/AuthorList/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Raphael Karanja on 2019-01-30.
+ */
+import React from 'react'
+
+const AuthorList = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AuthorList;
