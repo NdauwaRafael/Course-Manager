1cf4720cf19592800b989c3441f0d5ec788bfe86
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 15:52:17 +0300

    UPDATE:  Style action buttons

--- src/components/Course/Courses/CourseList/CourseRow/index.js
+++ src/components/Course/Courses/CourseList/CourseRow/index.js
@@ -10,12 +10,24 @@ export default  ({course})=>
         <td>{course.category && course.category.name}</td>
         <td>{course.author && course.author.name}</td>
         <td>
-            <Link to={'/course/'+course.id}>
-                <button
-                    className="bg-transparent hover:bg-green text-green-dark font-semibold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded">
-                    show
-                </button>
 
-            </Link>
+            <div className="inline-flex">
+                <Link to={'/course/'+course.id} className="bg-transparent hover:bg-green text-green-dark hover:text-green-lightest font-bold py-2 px-4 rounded">
+                    Show
+                </Link>
+                <Link to={'/course/'+course.id} className="bg-transparent hover:bg-blue text-blue-light hover:text-blue-lightest font-bold py-2 px-4 rounded">
+                    Edit
+                </Link>
+                <Link to={'/course/'+course.id} className="bg-transparent hover:bg-red text-red-light hover:text-red-lightest font-bold py-2 px-4 rounded">
+                    Delete
+                </Link>
+            </div>
+
+            {/*<Link to={'/course/'+course.id}>*/}
+                {/*<button*/}
+                    {/*className="bg-transparent hover:bg-green text-green-dark font-semibold hover:text-white py-2 px-4 border border-green hover:border-transparent rounded">*/}
+                    {/*show*/}
+                {/*</button>*/}
+            {/*</Link>*/}
         </td>
     </tr>
