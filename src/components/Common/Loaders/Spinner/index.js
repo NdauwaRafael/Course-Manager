8625c2d575901891b8d02f65eb637055b2022e83
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 08:34:02 +0300

    UPDATE:  iterate through a loop of spinner children divs

--- src/components/Common/Loaders/Spinner/index.js
+++ src/components/Common/Loaders/Spinner/index.js
@@ -4,9 +4,18 @@
 import React from 'react'
 
 const Spinner = (props) => {
-    return (
-        <div>
+    let children_spinners = '';
+    let i = 0;
+    while (i <= 12) {
+        children_spinners+='<div></div>';
+        i++;
+    }
 
+    return (
+        <div className="lds-css ng-scope">
+            <div className="lds-spinner" style="100%;height:100%">
+                {children_spinners}
+            </div>
         </div>
     )
 }
