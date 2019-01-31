84613300f90294689f89400a14f5620d12a5e90b
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 08:19:41 +0300

    UPDATE:  Add a spinner component

---
+++ src/components/Common/Loaders/Spinner/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Raphael Karanja on 2019-01-31.
+ */
+import React from 'react'
+
+const Spinner = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Spinner;
