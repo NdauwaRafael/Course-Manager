6151c2849f8e7aca215c88a1f5154936629c41fa
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 12:35:20 +0300

    UPDATE:  Add font awesome

--- src/components/Course/Courses/index.js
+++ src/components/Course/Courses/index.js
@@ -30,8 +30,10 @@ class CoursesPage extends Component {
                     <div className="">
                         <button className="bg-teal text-white font-bold py-2 px-4 rounded" onClick={this.createCoursePage}>
                             Add Course
+                            <i className="fa fa-spinner fa-pulse"> </i>
+                            {/*<i className="fas fa-spinner-third fa-pulse fa-fw"> </i>*/}
+                            {/*<i className="fas fa-circle-notch fa-pulse"> </i>*/}
                         </button>
-                        <Spinner />
                     </div>
                 </div>
                 <div className="mb-5">
