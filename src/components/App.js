89208e1f5b8c6ae71904c290eb6c3660d36e9a23
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 15:00:14 +0300

    Display spinner only when an ajax call initializes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,29 @@
-import React, { Component} from 'react';
+import React, {Component} from 'react';
 import Header from './Layout/Header';
 import Routes from '../routes';
 import Spinner from './Common/Loaders/Spinner';
 import {connect} from 'react-redux';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Header />
-          <Spinner />
-        <div className="container mx-auto pt-10">
-          {
-            Routes
-          }
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const {loading} = this.props;
+        return (
+            <div className="container-fluid">
+                <Header/>
+                {loading && <Spinner/>}
+                <div className="container mx-auto pt-10">
+                    {
+                        Routes
+                    }
+                </div>
+            </div>
+        );
+    }
 }
-const mapStateToProps = (state, ownProps)=>{
+
+const mapStateToProps = (state, ownProps) => {
     return {
         loading: state.ajaxCallsInProgress > 0
     }
 };
-export default connect( mapStateToProps)(App);
+export default connect(mapStateToProps)(App);
