11b52b7cf63ec58e8bafd4295aa6d50a75c5554c
Raphael Karanja <rkaranja@cytonn.com>
2019-01-31 14:13:01 +0300

    UPDATE:  Add ajaxStatusReducer reducer and custom methods in the reducer

--- src/CourseAppStore/reducers/AjuxStatusReducer/index.js
+++ src/CourseAppStore/reducers/AjuxStatusReducer/index.js
@@ -4,9 +4,15 @@
 import * as types from '../../actions/ActionTypes';
 import initialStates from '../InitialState';
 
-export default (state = initialStates.ajaxCallsInProgress, action)=> {
-    if(action.type === types.BEGIN_AJAX_CALL){
+const actionTypeEndsInSuccess = (type) => {
+    return type.substring(type.length - 8) === '_SUCCESS';
+};
+
+export default (state = initialStates.ajaxCallsInProgress, action) => {
+    if (action.type === types.BEGIN_AJAX_CALL) {
         return state + 1;
+    }else if(actionTypeEndsInSuccess(action.type)){
+        return state - 1;
     }
 
     return state;
