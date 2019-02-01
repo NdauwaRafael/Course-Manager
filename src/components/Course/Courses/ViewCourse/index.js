92296dbf24ae02951c918d166348ace084938745
Raphael Karanja <rkaranja@cytonn.com>
2019-02-01 10:29:00 +0300

    UPDATE:  add and style leave comment section

--- src/components/Course/Courses/ViewCourse/index.js
+++ src/components/Course/Courses/ViewCourse/index.js
@@ -22,6 +22,7 @@ class ViewCourse extends Component {
         const {course} = this.props;
         const {author, category} = course;
         return (
+            <div>
             <div className="w-full lg:flex">
                 <div
                     className=" bg-white rounded-b lg:rounded-b-none lg:rounded-l lg:rounded-r p-4 flex flex-col justify-between leading-normal">
@@ -54,6 +55,31 @@ class ViewCourse extends Component {
                             </div>
                     </div>
                 </div>
+            </div>
+
+
+                <div className="w-full lg:flex">
+                    <div
+                        className="border-r border-b border-l border-grey-light lg:border-l lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-l lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                        <div className="mb-8">
+                            <p className="text-sm text-grey-dark flex items-center">
+                                <svg className="fill-current text-grey w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path
+                                        d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/>
+                                </svg>
+                                <span>
+                                Leave a comment
+                            </span>
+                            </p>
+                                <div>
+
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         );
     }
