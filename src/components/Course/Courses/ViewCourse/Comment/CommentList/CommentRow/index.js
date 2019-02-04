ba7e6e91a24b33af3adffa0dc415550af931d598
Raphael Karanja <rkaranja@cytonn.com>
2019-02-04 16:43:13 +0300

    UPDATE:  add basic descriptions for a course row component

--- src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
+++ src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
@@ -1,3 +1,19 @@
 /**
  * Created by Raphael Karanja on 2019-02-04.
  */
+export default ({comment})=>{
+    return (
+        <div>
+            <div
+                className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div className="mb-8">
+                    <p className="text-sm text-grey-dark flex items-center">
+                        {comment.created_at}
+                    </p>
+                    <p className="text-grey-darker text-base">{comment.comment}</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
