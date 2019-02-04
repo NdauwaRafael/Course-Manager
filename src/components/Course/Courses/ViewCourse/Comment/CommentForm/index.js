6731120b09b55621e90498d9e28f5dc4cb2bdd2d
Raphael Karanja <rkaranja@cytonn.com>
2019-02-04 15:57:19 +0300

    UPDATE:  clear comment state once comment have been saved

--- src/components/Course/Courses/ViewCourse/Comment/CommentForm/index.js
+++ src/components/Course/Courses/ViewCourse/Comment/CommentForm/index.js
@@ -30,7 +30,7 @@ const CommentForm = ({onSave, comment, onChange, error}) => {
                                             error={error.comment}/>
 
                             <button
-                                className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded"
+                                className="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded mt-4"
                                 type="submit"
                                 onClick={onSave}>
                                 Save
