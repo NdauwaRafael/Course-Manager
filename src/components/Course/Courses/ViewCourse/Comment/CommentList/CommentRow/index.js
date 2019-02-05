e03bf25174d70567323cfb68fc6a8e6bfe8db860
Raphael Karanja <rkaranja@cytonn.com>
2019-02-05 11:38:40 +0300

    UPDATE:  add like buttons for comment bubble

--- src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
+++ src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
@@ -5,20 +5,37 @@ import React from 'react';
 
 export default ({comment})=>{
     return (
-        <div className="max-w-lg lg:flex shadow-inner mb-4 p-1 rounded-lg" >
+        <div className="max-w-lg lg:flex shadow-inner mb-4 p-1 rounded-lg border-b border-grey-light" >
             <div className="flex-none text-grey-dark text-center items-center justify-center flex flex-col text-2xl">
                 <i className="fa fa-comments-o text-2xl" aria-hidden="true"> </i>
             </div>
             <div
-                className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                <div className="">
+                className="max-w-lg bg-white rounded-b lg:rounded-b-none lg:rounded-r pl-4 p-1 flex flex-col justify-between leading-normal ">
+                <div className="mb-2">
                     <p className="text-xs text-grey-dark flex items-center">
                         {comment.created_at}
                     </p>
                     <p className="text-grey-darker text-base">{comment.comment}</p>
                 </div>
 
+                <div className="flex items-center lg:flex items-end items-stretch justify-start">
+                    <div className="text-sm flex ">
+                        <p className="mr-2 text-teal-light">
+                            <i className="fa fa-thumbs-o-up" aria-hidden="true"> </i>
+                        </p>
+
+                        {/*<p className="mr-2">*/}
+                            {/*<i className="fa fa-thumbs-o-down" aria-hidden="true"> </i>*/}
+                        {/*</p>*/}
+
+                        <p className="mr-2 text-teal-light">
+                            <i className="fa fa-heart-o" aria-hidden="true"> </i>
+                        </p>
+                    </div>
+                </div>
             </div>
+
+
         </div>
     )
 }
