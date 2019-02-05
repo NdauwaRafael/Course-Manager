7519311d70c584a8a6803a43a81063c811ea5ac5
Raphael Karanja <rkaranja@cytonn.com>
2019-02-05 13:07:40 +0300

    UPDATE:  add basic boiler plate for view author page

---
+++ src/components/Course/Authors/ViewAuthor/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Raphael Karanja on 2019-02-05.
+ */
+import React, {Component} from 'react';
+class ViewAuthor extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+
+}
