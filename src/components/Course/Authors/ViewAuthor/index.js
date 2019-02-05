42c84c27cb57e536496948d0dcf7ecb0584f03b8
Raphael Karanja <rkaranja@cytonn.com>
2019-02-05 15:06:59 +0300

    UPDATE:  return author and course from page param

--- src/components/Course/Authors/ViewAuthor/index.js
+++ src/components/Course/Authors/ViewAuthor/index.js
@@ -2,6 +2,7 @@
  * Created by Raphael Karanja on 2019-02-05.
  */
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 class ViewAuthor extends Component{
     constructor(props) {
         super(props);
@@ -11,9 +12,49 @@ class ViewAuthor extends Component{
     render() {
         return(
             <div>
-                
+
             </div>
         );
     }
 
 }
+
+const getAuthorById = (authors, id)=>{
+    let pageAuthor = authors.filter(author=>parseInt(author.id) === parseInt(id));
+    if(pageAuthor.length > 0){
+        return pageAuthor[0]
+    }
+    return {};
+};
+
+const getCoursesByAuthorId = (courses, id)=>{
+    let pageCourses = courses.filter(course => parseInt(course.authorId) === parseInt(id));
+    if (pageCourses.length > 0){
+        return pageCourses[0]
+    }
+
+    return []
+}
+const mapStateToProps = (state, ownProps)=>{
+            let author = {};
+            let courses = []
+            let authorId = ownProps.match.params.id;
+            if(authorId && state.authors){
+                author = getAuthorById(state.authors, authorId);
+                if(state.courses){
+                    courses = getCoursesByAuthorId(state.courses, authorId)
+                }
+            }
+        return {
+            author: author
+        }
+};
+
+const mapDispatchToProps = (dispatch)=>{
+
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ViewAuthor)
