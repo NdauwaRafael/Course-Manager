a3ee7c0c868e685beb7228bec4eb526e573ca576
Raphael Karanja <rkaranja@cytonn.com>
2019-02-05 08:54:19 +0300

    UPDATE:  style comment cards

--- src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
+++ src/components/Course/Courses/ViewCourse/Comment/CommentList/CommentRow/index.js
@@ -5,11 +5,14 @@ import React from 'react';
 
 export default ({comment})=>{
     return (
-        <div>
+        <div className="w-full lg:flex shadow mb-4 p-1">
+            <div className="flex-none text-grey-dark text-center items-center justify-center flex flex-col">
+                <i className="fa fa-comments-o" aria-hidden="true"> </i>
+            </div>
             <div
                 className=" bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                 <div className="mb-8">
-                    <p className="text-sm text-grey-dark flex items-center">
+                    <p className="text-xs text-grey-dark flex items-center">
                         {comment.created_at}
                     </p>
                     <p className="text-grey-darker text-base">{comment.comment}</p>
